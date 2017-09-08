1bd588df5414ee5ac7b773920b14900f8fe8549b
havishat <sundariha@gmail.com>
2017-09-07 17:03:34 -0700

    add

--- coldcall/ViewController.swift
+++ coldcall/ViewController.swift
@@ -10,10 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var coldcallbutton: UIButton!
+    @IBOutlet weak var coldcallLabel: UILabel!
+    
+    var coldword = [
+    "Jay","Bryant","Cody","Jimmy"
+    ]
+    var currentcoldword = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+//        updateUI()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func updateUI(){
+        coldcallLabel.text = coldword[currentcoldword]
+    }
+    
+//    func shuffle(){
+//        for _ in 0...10 {
+//            let random = Int(arc4random_uniform(UInt32(coldword.count)))
+//            let random2 = Int(arc4random_uniform(UInt32(coldword.count)))
+//            if random != random2 {
+//                swap(&coldword[random], &coldword[random2])
+//            }
+//        }
+//    }
+    
+    @IBAction func coldcallbutton(_ sender: UIButton) {
+        if currentcoldword != coldword.count - 1 {
+            currentcoldword = Int(arc4random_uniform(UInt32(coldword.count)))
+        }else{
+            currentcoldword = 0
+        }
+        
+        updateUI()
+//        shuffle()
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
